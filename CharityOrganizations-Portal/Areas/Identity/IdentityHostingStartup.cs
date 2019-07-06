a461680718cf4271335e1945af5204f0d881272f
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2019-07-06 02:19:11 +0200

    Add project files.

---
+++ CharityOrganizations-Portal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CharityOrganizations_Portal.Areas.Identity.Data;
+using CharityOrganizations_Portal.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CharityOrganizations_Portal.Areas.Identity.IdentityHostingStartup))]
+namespace CharityOrganizations_Portal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UsersContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<PortalUser>()
+                    .AddEntityFrameworkStores<UsersContext>();
+            });
+        }
+    }
+}
